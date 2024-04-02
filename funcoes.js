7e3485f6c6cd18540d3aed183542d683afa02d61
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-04-02 10:16:30 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,28 @@
+function imprimeTexto (texto) {
+    console.log (texto);
+}
+
+imprimeTexto ( "oi amigos")//Passar o parametro para função retornar um valor.
+ 
+function texto (text){
+   return text;
+}
+console.log (texto("parametro"))
+
+function soma (num1,num2){
+    return num1 + num2;
+}
+function subtracao ( num1,num2){
+    return num1 - num2;
+}
+function divisao ( num1,num2){
+    return num1/num2;
+}
+function mutiplicacao ( num1,num2){
+    return  num1 * num2;
+}
+console.log ("A soma dos numeros é",soma (3,2))
+console.log ("A subtracao dos numeros é",subtracao (3,2))
+console.log ("A Divisão dos números é ", divisao (3,2))
+console.log ("A multiplicação dos números é", mutiplicacao (3,2))
+
